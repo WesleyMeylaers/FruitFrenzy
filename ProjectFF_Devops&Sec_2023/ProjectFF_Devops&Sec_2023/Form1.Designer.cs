a08ca97a0a14f4c72331da2507ba908b6f87a51c
WesleyMeylaers <r0929439@student.thomasmore.be>
2023-11-17 18:53:47 +0100

    KleurenTitel

--- ProjectFF_Devops&Sec_2023/ProjectFF_Devops&Sec_2023/Form1.Designer.cs
+++ ProjectFF_Devops&Sec_2023/ProjectFF_Devops&Sec_2023/Form1.Designer.cs
@@ -28,10 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.LoginPanel = new System.Windows.Forms.Panel();
             this.NNlabel1 = new System.Windows.Forms.Label();
             this.UsernameTXTBOX = new System.Windows.Forms.TextBox();
             this.LoginBtn = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.LoginPanel.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -41,7 +45,7 @@
             this.LoginPanel.Controls.Add(this.LoginBtn);
             this.LoginPanel.Controls.Add(this.UsernameTXTBOX);
             this.LoginPanel.Controls.Add(this.NNlabel1);
-            this.LoginPanel.Location = new System.Drawing.Point(494, 233);
+            this.LoginPanel.Location = new System.Drawing.Point(1044, 442);
             this.LoginPanel.Name = "LoginPanel";
             this.LoginPanel.Size = new System.Drawing.Size(352, 151);
             this.LoginPanel.TabIndex = 0;
@@ -77,18 +81,39 @@
             this.LoginBtn.UseVisualStyleBackColor = false;
             this.LoginBtn.Click += new System.EventHandler(this.LoginBtn_Click);
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(126)))), ((int)(((byte)(175)))), ((int)(((byte)(52)))));
+            this.panel1.Location = new System.Drawing.Point(252, 124);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(658, 420);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(249, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(479, 79);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Fruit Frenzy";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(100)))), ((int)(((byte)(66)))));
             this.ClientSize = new System.Drawing.Size(1482, 819);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.LoginPanel);
             this.Name = "Form1";
             this.Text = "Form1";
             this.LoginPanel.ResumeLayout(false);
             this.LoginPanel.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -98,6 +123,9 @@
         private System.Windows.Forms.TextBox UsernameTXTBOX;
         private System.Windows.Forms.Label NNlabel1;
         private System.Windows.Forms.Button LoginBtn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- ProjectFF_Devops&Sec_2023/ProjectFF_Devops&Sec_2023/Form1.cs
+++ ProjectFF_Devops&Sec_2023/ProjectFF_Devops&Sec_2023/Form1.cs
@@ -12,14 +12,56 @@ namespace ProjectFF_Devops_Sec_2023
 {
     public partial class Form1 : Form
     {
+        private Color currentColor;
+        private readonly Random random = new Random();
         public Form1()
         {
             InitializeComponent();
-        }
 
+            currentColor = Color.Black;
+            Task.Run(() => ContinuousColorChange());
+        }
+        private async Task ContinuousColorChange()
+        {
+            while (true)
+            {
+                // Generate a new random color for the label's forecolor
+                Color newColor = Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+
+                // Transition smoothly from the current color to the new color
+                int steps = 10; // Number of steps for the transition
+
+                for (int i = 0; i <= steps; i++)
+                {
+                    int r = (currentColor.R * (steps - i) + newColor.R * i) / steps;
+                    int g = (currentColor.G * (steps - i) + newColor.G * i) / steps;
+                    int b = (currentColor.B * (steps - i) + newColor.B * i) / steps;
+
+                    // Update the label's forecolor
+                    ChangeColor(Color.FromArgb(r, g, b));
+
+                    // Pause briefly to create a smooth transition effect
+                    await Task.Delay(50);
+                }
+
+                // Update the current color for the next iteration
+                currentColor = newColor;
+            }
+        }
         private void LoginBtn_Click(object sender, EventArgs e)
         {
             LoginPanel.Visible = false;
+        }
+        private void ChangeColor(Color color)
+        {
+            if (label1.InvokeRequired)
+            {
+                label1.Invoke((MethodInvoker)(() => label1.ForeColor = color));
+            }
+            else
+            {
+                label1.ForeColor = color;
+            }
         }
     }
 }
